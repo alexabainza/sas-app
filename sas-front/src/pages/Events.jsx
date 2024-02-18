80e8088d5e119e5eafce30cfbb723bf1cd8d4f72
Alex Abainza <abainza.alexandra@gmail.com>
2024-02-18 12:32:18 +0800

    events page template, fixed navbar at smallest screen size

--- sas-front/src/pages/Events.jsx
+++ sas-front/src/pages/Events.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import EventCard from "../components/EventCard";
 
 const Events = () => {
   return (
-    <div>Events</div>
-  )
-}
+    <>
+      <h1 className="text-center text-5xl lg:text-5xl md:text-4xl sm:text-3xl font-semibold pt-5 lg:pt-8 md:pt-6 sm:pt-4 pb-4 lg:pb-4 md:pb-3 sm:pb-2">Events</h1>
+      <div className="mb-2 sm:mb-1 py-2 sm:py-1 mx-5 sm:mx-16 px-4 sm:px-1 flex flex-wrap">
+        <EventCard />
+        <EventCard />
+        <EventCard />
+      </div>
+      
+    </>
+  );
+};
 
-export default Events
+export default Events;
--- sas-front/src/pages/Home.jsx
+++ sas-front/src/pages/Home.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='text-blue-700'>Home</h1></div>
-  )
-}
+    
+    <body className="">
+      <h1 className="text-blue-700">Home</h1>
+      <p>Adsa</p>
+    </body>
+  );
+};
 
-export default Home
+export default Home;
